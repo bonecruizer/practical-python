406c82805eb5fe727ab0356d1c2ba488f5134fc1
bonecruizer <77049508+bonecruizer@users.noreply.github.com>
2021-02-17 13:40:18 +0100

    commit to complete

--- Work/pcost.py
+++ Work/pcost.py
@@ -1,3 +1,21 @@
 # pcost.py
 #
 # Exercise 1.27
+portfolio = open('Data/portfolio.csv','rt')
+headers = next(portfolio)
+
+currentline = []
+totalcost = 0
+
+for line in portfolio:
+    currentline = line.split(',')
+    stockcost = int(currentline[1]) * float(currentline[2])
+    print(f'Cost of: {currentline[0]} ,=  {currentline[1]}, x  {currentline[2].strip()} = {stockcost:.2f}')
+    totalcost = totalcost + stockcost
+
+
+
+
+portfolio.close()
+
+print(f'Total Cost: {totalcost}')
